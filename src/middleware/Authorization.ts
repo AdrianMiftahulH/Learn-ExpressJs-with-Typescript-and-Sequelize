f82f927e33550465c4a41b60c0a319f6eca72cb5
AdrianMiftahulH <adrianmiftahul@gmail.com>
2023-03-14 14:14:37 +0700

    authorizaton User

--- src/middleware/Authorization.ts
+++ src/middleware/Authorization.ts
@@ -4,9 +4,9 @@ import Helper from "../helpers/Helper";
 const Authenticated = (req: Request, res: Response, next: NextFunction) => {
     try {
         // membuat var authToken dengan value request dari header dengan name authorization
-        const authToken = req.headers["authorization"]
+        const authToken = req.headers["authorization"];
         // membuat var token dengan value authToken dan authToken yang di split(data string yang dibagi menjadi array dan di return arra baru) ;au memilih array 1
-        const token = authToken && authToken.split(" ")[1];
+		const token = authToken && authToken.split(" ")[1];
 
         // kondisi token null
         if (token === null) {
@@ -17,10 +17,48 @@ const Authenticated = (req: Request, res: Response, next: NextFunction) => {
             return res.status(401).send(Helper.ResponseData(401, "Unautorized", null, null));
         }
         res.locals.userId = result?.email;
+        res.locals.roleId = result?.roleId;
         next();
     } catch (err: any) {
         return res.status(500).send(Helper.ResponseData(500, "", err, null));
     }
 }
 
-export default { Authenticated }
+const SuperUser = (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const roleId = res.locals.roleId;
+        if(roleId !== 1){
+            return res.status(403).send(Helper.ResponseData(403, "Forbiden", null, null));
+        }
+
+        next();
+    } catch (err: any) {
+        return res.status(500).send(Helper.ResponseData(500, "", err, null));
+    }
+}
+const AdminRole = (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const roleId = res.locals.roleId;
+        if(roleId !== 2){
+            return res.status(403).send(Helper.ResponseData(403, "Forbiden", null, null));
+        }
+
+        next();
+    } catch (err: any) {
+        return res.status(500).send(Helper.ResponseData(500, "", err, null));
+    }
+}
+const BasicUser = (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const roleId = res.locals.roleId;
+        if(roleId !== 3){
+            return res.status(403).send(Helper.ResponseData(403, "Forbiden", null, null));
+        }
+
+        next();
+    } catch (err: any) {
+        return res.status(500).send(Helper.ResponseData(500, "", err, null));
+    }
+}
+
+export default { Authenticated, SuperUser, AdminRole, BasicUser }
--- src/router/Routes.ts
+++ src/router/Routes.ts
@@ -13,13 +13,13 @@ const router = express.Router();
 // Menambahakan middleware Authorization untuk user bila ingin akses tapi belom login maka tidak bisa
 router.get("/role", Authorization.Authenticated , RoleController.GetRole);
 // membuat sebuah data
-router.post("/role", RoleController.CreateRole);
+router.post("/role",Authorization.Authenticated, Authorization.AdminRole, RoleController.CreateRole);
 // mengupdate data sesuai id
-router.post("/role/:id", RoleController.UpdateRole);
+router.post("/role/:id",Authorization.Authenticated, Authorization.AdminRole, RoleController.UpdateRole);
 // menghapus data sesuai id
-router.delete("/role/:id", RoleController.DeleteRole);
+router.delete("/role/:id", Authorization.Authenticated, Authorization.SuperUser , RoleController.DeleteRole);
 // memanggil data sesuai id
-router.get("/role/:id", RoleController.GetRoleById);
+router.get("/role/:id",Authorization.Authenticated, RoleController.GetRoleById);
 
 // Router Register
 // Jadi kegunaan middleware itu untuk memvalidasi dahulu bila berhasil maka akan dilanjutkan ke Controller. Bila tidak akan di stop di middleware tidak akan di lanjutkan ke controller
